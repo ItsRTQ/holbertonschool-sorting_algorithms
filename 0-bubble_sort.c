2e030cadd705cd12955099504a0d3f67b33cc543
William <titanbabrisa@gmail.com>
2024-01-16 17:33:36 -0400

    added bubble sorting logic

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,5 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	/*implement logic*/
+	int temp;
+	size_t i, j;
+
+	if (size >= 2 && array != NULL)
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
+			for (j = 0; j < (size - i - 1); j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
 }
