5e1c51643309e33bbbc401b4cbfc903f65090395
William <titanbabrisa@gmail.com>
2024-01-16 19:25:06 -0400

    insertion sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * insertion_sort_list - sort given list using insertion
  * @list: targeted link list
@@ -7,5 +8,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	/*implement logic*/
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	listint_t *curr = (*list)->next, *temp;
+
+	while (curr != NULL)
+	{
+		temp = curr;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			temp->next->prev = temp;
+			print_list(*list);
+		}
+		curr = curr->next;
+	}
 }
