740fe54db8850f5c331c6ddcb52b0b55e99c14dd
William <titanbabrisa@gmail.com>
2024-01-16 16:31:57 -0400

    added files & template

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort given list using insertion
+ * @list: targeted link list
+ *
+ * Return: void, doesnt return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	/*implement logic*/
+}
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,12 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort data using bubble sorting
+ * quick_sort - sort using quick sorting
  * @array: targeted array to sort
  * @size: size of the targeted array
- *
  * Return: void, doesnt return
  */
-void bubble_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
 	/*implement logic*/
 }
